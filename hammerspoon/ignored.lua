9d4291e49ee66394fce6f01049e049d868767f5c
whiledoing <whiledoing@163.com>
2016-04-15 09:05:09 +0800

    change hammerspoon config

--- hammerspoon/ignored.lua
+++ hammerspoon/ignored.lua
@@ -1,19 +1,17 @@
+-- 设置不可以移动的app名称
 local ignored_apps = {
-	'QQ',
-	'微信',
-	'Newsflow',
-	'SourceTree',
-    'iTerm'
+    'QQ',
+    '微信',
 }
 
 function ignored(win)
-	local app = win:application()
-	if app then
-		local title = app:title()
-		print('identifier'..title)
-		return hs.fnutils.contains(ignored_apps, title)
-	end
-	return false
+    local app = win:application()
+    if app then
+	local title = app:title()
+	print('identifier'..title)
+	return hs.fnutils.contains(ignored_apps, title)
+    end
+    return false
 end
 
 return ignored
--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -13,18 +13,9 @@ if not hs.ipc.cliStatus() then hs.ipc.cliInstall() end
 
 -- App shortcuts
 local key2App = {
-    q = 'QQ',
     w = 'Safari',
-    x = 'Xcode',
-    j = 'AppCode',
-    a = 'Android Studio',
-    s = 'Sublime Text 2',
-    g = 'SourceTree',
-    p = 'Adobe Photoshop CC',
     f = 'Finder',
     t = 'iTerm',
-    n = 'Newsflow',
-    c = 'Numi'
 }
 for key, app in pairs(key2App) do
     hs.hotkey.bind(hyper, key, function() hs.application.launchOrFocus(app) end)
@@ -34,6 +25,20 @@ end
 hs.hotkey.bind(hyper, '.', function() 
     hs.hints.windowHints(getAllValidWindows()) 
 end)
+
+function getAllValidWindows ()
+    local allWindows = hs.window.allWindows()
+    local windows = {}
+    local index = 1
+    for i = 1, #allWindows do
+        local w = allWindows[i]
+        if w:screen() then
+            windows[index] = w
+            index = index + 1
+        end
+    end
+    return windows
+end
 
 -- undo
 local undo = require 'undo'
@@ -48,11 +53,10 @@ hs.grid.MARGINY  = 0
 local moveMaxWidth = hs.grid.GRIDWIDTH / 2 + 1
 local moveMinWidth = hs.grid.GRIDWIDTH / 2 - 1
 
-local hyperUp = hs.hotkey.bind(hyper, 'up', function() 
+local hyperUp = hs.hotkey.bind(hyper, 'k', function() 
     undo:addToStack()
     hs.grid.maximizeWindow() 
 end)
-
 
 -- Move Window
 function horizontalMove(direction)
@@ -84,236 +88,221 @@ function horizontalMove(direction)
             g.w = moveMaxWidth
             g.x = 0
             toMove = true
-        elseif g.w >= moveMinWidth then
-            g.x = hs.grid.GRIDWIDTH - g.w
-            toMove = true
+            elseif g.w >= moveMinWidth then
+                g.x = hs.grid.GRIDWIDTH - g.w
+                toMove = true
+            end
+            if toMove then
+                undo:addToStack()
+                hs.grid.set(w, g, s)
+                if direction > 0 and g.x + g.w >= hs.grid.GRIDWIDTH then
+                    w:ensureIsInScreenBounds()
+                end
+            end
+            return
         end
-        if toMove then
+
+        if g.x == 0 then
+            g.w = g.w + direction
+            local toMove = false
+            if g.w > moveMaxWidth then
+                g.w = moveMaxWidth
+                g.x = hs.grid.GRIDWIDTH - moveMaxWidth
+                toMove = true
+                elseif g.w >= moveMinWidth then
+                    toMove = true
+                end
+                if toMove then
+                    undo:addToStack()
+                    hs.grid.set(w, g, s)
+                end
+                return
+            end
+
+            g.w = hs.grid.GRIDWIDTH / 2
+            g.x = direction > 0 and hs.grid.GRIDWIDTH / 2 or 0
             undo:addToStack()
             hs.grid.set(w, g, s)
-            if direction > 0 and g.x + g.w >= hs.grid.GRIDWIDTH then
-                w:ensureIsInScreenBounds()
+        end
+
+        local hyperLeft = hs.hotkey.bind(hyper, 'h', function() horizontalMove(-1) end)
+        local hyperRight = hs.hotkey.bind(hyper, 'l', function() horizontalMove(1) end)
+
+        -- Move Screen
+        hs.hotkey.bind(hyperShift, 'h', function() 
+            local w = hs.window.focusedWindow()
+            if not w then 
+                return
+            end
+            if ignored(w) then return end
+
+            local s = w:screen():toWest()
+            if s then
+                undo:addToStack()
+                w:moveToScreen(s)
+            end
+        end)
+
+        hs.hotkey.bind(hyperShift, 'l', function() 
+            local w = hs.window.focusedWindow()
+            if not w then 
+                return
+            end
+            if ignored(w) then return end
+
+            local s = w:screen():toEast()
+            if s then
+                undo:addToStack()
+                w:moveToScreen(s)
+            end
+        end)
+
+        -- split view
+        SplitModal = require 'split_modal'
+        local splitModal = SplitModal.new(hyperShift, 's', undo)
+
+        function splitModal:hotkeysToDisable()
+            return {hyperUp, hyperRight, hyperLeft}
+        end
+
+        -- App layout
+        local AppLayout = {}
+        AppLayout['QQ'] = { small = true }
+        AppLayout['Safari'] = { small = true, full = true }
+        AppLayout['Sublime Text 2'] = { small = true }
+        AppLayout['SourceTree'] = { small = true }
+        AppLayout['Finder'] = { small = true, full = true}
+        AppLayout['iTerm'] = { small = true }
+        AppLayout['Newsflow'] = { small = true }
+        AppLayout['Xcode'] = { small = true, full = true, delay = 10 }
+        AppLayout['AppCode'] = { large = true, full = true, delay = 10 }
+        AppLayout['Android Studio'] = { large = true, full = true, delay = 10 }
+
+        function layoutApp(name, app, delayed)
+            local conf = AppLayout[name]
+            if not conf then return end
+
+            if not delayed and conf.delay then
+                print('delay layout '..name..' for '..conf.delay..' secs')
+                hs.timer.doAfter(conf.delay, function() 
+                    layoutApp(name, app, true)
+                end)
+                return
+            end
+
+            if not app then app = hs.appfinder.appFromName(name) end
+            local w = nil
+            if app then w = app:mainWindow() end
+            if not w then return end
+            local s = nil
+            if conf.small then
+                print('move app '..name..' to smallerScreen')
+                s = smallerScreen
+            end
+            if conf.large then
+                print('move app '..name..' to largerScreen')
+                s = largerScreen
+            end
+            if s and w:screen() and s ~= w:screen() then w:moveToScreen(s) end
+
+            if conf.full and w:screen() then
+                print('maximize app '..name)
+                w:maximize()
             end
         end
-        return
-    end
 
-    if g.x == 0 then
-        g.w = g.w + direction
-        local toMove = false
-        if g.w > moveMaxWidth then
-            g.w = moveMaxWidth
-            g.x = hs.grid.GRIDWIDTH - moveMaxWidth
-            toMove = true
-        elseif g.w >= moveMinWidth then
-            toMove = true
-        end
-        if toMove then
-            undo:addToStack()
-            hs.grid.set(w, g, s)
-        end
-        return
-    end
-
-    g.w = hs.grid.GRIDWIDTH / 2
-    g.x = direction > 0 and hs.grid.GRIDWIDTH / 2 or 0
-    undo:addToStack()
-    hs.grid.set(w, g, s)
-end
-
-local hyperLeft = hs.hotkey.bind(hyper, 'h', function() horizontalMove(-1) end)
-
-local hyperRight = hs.hotkey.bind(hyper, 'l', function() horizontalMove(1) end)
-
--- Move Screen
-hs.hotkey.bind(hyperShift, 'h', function() 
-    local w = hs.window.focusedWindow()
-    if not w then 
-        return
-    end
-    if ignored(w) then return end
-
-    local s = w:screen():toWest()
-    if s then
-        undo:addToStack()
-        w:moveToScreen(s)
-    end
-end)
- 
-hs.hotkey.bind(hyperShift, 'l', function() 
-    local w = hs.window.focusedWindow()
-    if not w then 
-        return
-    end
-    if ignored(w) then return end
-    
-    local s = w:screen():toEast()
-    if s then
-        undo:addToStack()
-        w:moveToScreen(s)
-    end
-end)
-
--- split view
-SplitModal = require 'split_modal'
-local splitModal = SplitModal.new(hyper, 'down', undo)
-
-function splitModal:hotkeysToDisable()
-    return {hyperUp, hyperRight, hyperLeft}
-end
-
--- App layout
-local AppLayout = {}
-AppLayout['QQ'] = { small = true }
-AppLayout['Safari'] = { small = true, full = true }
-AppLayout['Sublime Text 2'] = { small = true }
-AppLayout['SourceTree'] = { small = true }
-AppLayout['Finder'] = { small = true }
-AppLayout['iTerm'] = { small = true }
-AppLayout['Newsflow'] = { small = true }
-AppLayout['Xcode'] = { small = true, full = true, delay = 10 }
-AppLayout['AppCode'] = { large = true, full = true, delay = 10 }
-AppLayout['Android Studio'] = { large = true, full = true, delay = 10 }
-
-function layoutApp(name, app, delayed)
-    local conf = AppLayout[name]
-    if not conf then return end
-
-    if not delayed and conf.delay then
-        print('delay layout '..name..' for '..conf.delay..' secs')
-        hs.timer.doAfter(conf.delay, function() 
-                layoutApp(name, app, true)
-            end)
-        return
-    end
-    
-    if not app then app = hs.appfinder.appFromName(name) end
-    local w = nil
-    if app then w = app:mainWindow() end
-    if not w then return end
-    local s = nil
-    if conf.small then
-        print('move app '..name..' to smallerScreen')
-        s = smallerScreen
-    end
-    if conf.large then
-        print('move app '..name..' to largerScreen')
-        s = largerScreen
-    end
-    if s and w:screen() and s ~= w:screen() then w:moveToScreen(s) end
-
-    if conf.full and w:screen() then
-        print('maximize app '..name)
-        w:maximize()
-    end
-end
-
-function applicationWatcherCallback(name, event, app)
-    if event == hs.application.watcher.launched then
-        layoutApp(name, app)
-    end
-end
-
-local appWatcher = hs.application.watcher.new(applicationWatcherCallback)
-appWatcher:start()
-
--- screen change
-function screenChanged()
-    local ss = hs.screen.allScreens()
-    local count = #ss
-    if count ~= lastNumberOfScreens then
-        lastNumberOfScreens = count
-        local largest = 0
-        for i = 1, lastNumberOfScreens do
-            local s = ss[i]
-            local size = s:frame().w * s:frame().h
-            local preSmall = smallerScreen
-            smallerScreen = s
-            if size > largest then
-                largest = size
-                largerScreen = s
-                if preSmall then smallerScreen = preSmall end
+        function applicationWatcherCallback(name, event, app)
+            if event == hs.application.watcher.launched then
+                layoutApp(name, app)
             end
         end
-        print('NumberOfScreens '.. lastNumberOfScreens)
-        for app, conf in pairs(AppLayout) do
-            layoutApp(app, nil, true)
+
+        local appWatcher = hs.application.watcher.new(applicationWatcherCallback)
+        appWatcher:start()
+
+        -- screen change
+        function screenChanged()
+            local ss = hs.screen.allScreens()
+            local count = #ss
+            if count ~= lastNumberOfScreens then
+                lastNumberOfScreens = count
+                local largest = 0
+                for i = 1, lastNumberOfScreens do
+                    local s = ss[i]
+                    local size = s:frame().w * s:frame().h
+                    local preSmall = smallerScreen
+                    smallerScreen = s
+                    if size > largest then
+                        largest = size
+                        largerScreen = s
+                        if preSmall then smallerScreen = preSmall end
+                    end
+                end
+                print('NumberOfScreens '.. lastNumberOfScreens)
+                for app, conf in pairs(AppLayout) do
+                    layoutApp(app, nil, true)
+                end
+            end
         end
-    end
-end
 
-screenChanged()
+        screenChanged()
 
-local screenWatcher = hs.screen.watcher.new(screenChanged)
-screenWatcher:start()
+        local screenWatcher = hs.screen.watcher.new(screenChanged)
+        screenWatcher:start()
 
--- caffeinate
-hs.hotkey.bind(hyperShift, 'c', function() 
-    local c = hs.caffeinate
-    if not c then return end
-    if c.get('displayIdle') or c.get('systemIdle') or c.get('system') then
-        if menuCaff then
-            menuCaffRelease()
-        else
-            addMenuCaff()
-            local type
-            if c.get('displayIdle') then type = 'displayIdle' end
-            if c.get('systemIdle') then type = 'systemIdle' end
-            if c.get('system') then type = 'system' end
-            hs.alert('Caffeine already on for '..type)
+        -- caffeinate
+        hs.hotkey.bind(hyperShift, 'c', function() 
+            local c = hs.caffeinate
+            if not c then return end
+            if c.get('displayIdle') or c.get('systemIdle') or c.get('system') then
+                if menuCaff then
+                    menuCaffRelease()
+                else
+                    addMenuCaff()
+                    local type
+                    if c.get('displayIdle') then type = 'displayIdle' end
+                    if c.get('systemIdle') then type = 'systemIdle' end
+                    if c.get('system') then type = 'system' end
+                    hs.alert('Caffeine already on for '..type)
+                end
+            else
+                acAndBatt = hs.battery.powerSource() == 'Battery Power'
+                c.set('system', true, acAndBatt)
+                hs.alert('Caffeinated '..(acAndBatt and '' or 'on AC Power'))
+                addMenuCaff()
+            end
+        end)
+
+        function addMenuCaff()
+            menuCaff = hs.menubar.new()
+            menuCaff:setIcon("~/.hammerspoon/caffeine-on.pdf") 
+            menuCaff:setClickCallback(menuCaffRelease)
         end
-    else
-        acAndBatt = hs.battery.powerSource() == 'Battery Power'
-        c.set('system', true, acAndBatt)
-        hs.alert('Caffeinated '..(acAndBatt and '' or 'on AC Power'))
-        addMenuCaff()
-    end
-end)
 
-function addMenuCaff()
-    menuCaff = hs.menubar.new()
-    menuCaff:setIcon("~/.hammerspoon/caffeine-on.pdf") 
-    menuCaff:setClickCallback(menuCaffRelease)
-end
-
-function menuCaffRelease()
-    local c = hs.caffeinate
-    if not c then return end
-    if c.get('displayIdle') then
-        c.set('displayIdle', false, true)
-    end
-    if c.get('systemIdle') then
-        c.set('systemIdle', false, true)
-    end
-    if c.get('system') then
-        c.set('system', false, true)
-    end
-    if menuCaff then
-        menuCaff:delete()
-        menuCaff = nil
-    end
-    hs.alert('Decaffeinated')
-end
-
--- console
-hs.hotkey.bind(hyperShift, ';', hs.openConsole)
-
--- reload
-hs.hotkey.bind(hyper, 'escape', function() hs.reload() end )
-
--- utils
-function getAllValidWindows ()
-    local allWindows = hs.window.allWindows()
-    local windows = {}
-    local index = 1
-    for i = 1, #allWindows do
-        local w = allWindows[i]
-        if w:screen() then
-            windows[index] = w
-            index = index + 1
+        function menuCaffRelease()
+            local c = hs.caffeinate
+            if not c then return end
+            if c.get('displayIdle') then
+                c.set('displayIdle', false, true)
+            end
+            if c.get('systemIdle') then
+                c.set('systemIdle', false, true)
+            end
+            if c.get('system') then
+                c.set('system', false, true)
+            end
+            if menuCaff then
+                menuCaff:delete()
+                menuCaff = nil
+            end
+            hs.alert('Decaffeinated')
         end
-    end
-    return windows
-end
 
-hs.alert('Hammerspoon reload success', 0.8)
+        -- console
+        hs.hotkey.bind(hyperShift, ';', hs.openConsole)
+
+        -- reload
+        hs.hotkey.bind(hyper, 'escape', function() hs.reload() end )
+
+        -- reload success alert
+        hs.alert('Hammerspoon reload success', 0.8)
