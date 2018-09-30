dc052b6746c344950a4bec013bae36db97399033
whiledoing <whiledoing@163.com>
2018-09-30 20:53:41 +0800

    install tmux plugin on the first time

--- script/system_startup.sh
+++ script/system_startup.sh
@@ -75,5 +75,6 @@ fi
 if [ ! -d "$HOME/.tmux/plugins/tpm" ]; then
     echo "installing tmux plugin manager ..."
     git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+    tmux run-shell "$HOME/.tmux/plugins/tpm/bindings/update_plugins"
 fi
 
