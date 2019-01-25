dcaf58044d6db54a255cb5a2e014966b0520f6af
whiledoing <whiledoing@163.com>
2019-01-25 13:50:40 +0800

    switch brew mirror script

---
+++ script/swtich_brew_mirror.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+
+read -p "switch brew mirror site, please enter switch to origin(Y) or ustc(N): " yn
+
+function switch_to_ustc() {
+    # brew
+    cd "$(brew --repo)"
+    git remote set-url origin https://mirrors.ustc.edu.cn/brew.git
+
+    # brew-core
+    cd "$(brew --repo)/Library/Taps/homebrew/homebrew-core"
+    git remote set-url origin https://mirrors.ustc.edu.cn/homebrew-core.git
+
+    # brew-cask
+    cd "$(brew --repo)"/Library/Taps/homebrew/homebrew-cask
+    git remote set-url origin https://mirrors.ustc.edu.cn/homebrew-cask.git
+}
+
+function switch_to_origin() {
+    # brew
+    cd "$(brew --repo)"
+    git remote set-url origin https://github.com/Homebrew/brew.git
+
+    # brew-core
+    cd "$(brew --repo)/Library/Taps/homebrew/homebrew-core"
+    git remote set-url origin https://github.com/Homebrew/homebrew-core
+
+    # brew-cask
+    cd "$(brew --repo)"/Library/Taps/homebrew/homebrew-cask
+    git remote set-url origin https://github.com/Homebrew/homebrew-cask
+}
+
+case $yn in
+    [Yy]* ) switch_to_ustc; echo "Switch to ustc done!";;
+    [Nn]* ) switch_ro_origin; echo "Switch to origin done!";;
+    * ) echo "Nothing changed!"; exit;;
+esac
