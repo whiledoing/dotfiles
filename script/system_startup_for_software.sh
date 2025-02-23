7dc9e458e4563d67bdbf5bd055f41aa3d3f4f432
whiledoing <whiledoing@163.com>
2025-02-23 17:57:58 +0800

    update

--- script/system_startup_for_software.sh
+++ script/system_startup_for_software.sh
@@ -4,7 +4,7 @@
 [[ $(uname -s) = "Darwin" ]] || exit
 
 # install brew if not exists
-if [ ! $(command -v brew) ]; then
+if [[ ! $(command -v brew) ]]; then
     /usr/bin/ruby -e $(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)
 fi
 
@@ -13,10 +13,12 @@ brew_list=$(brew list)
 function update_module {
     for name in $brew_list; do
         if [ "$name" = "$1" ]; then
-            echo "[INFO] - $(date +%T) - already exists brew module [$1]."
+            echo "[INFO] - $(date +%T) - already exists brew module, upgrade it [$1]."
+            brew upgrade "$1"
             return
         fi
     done
+    echo "[INFO] - $(date +%T) - install module [$1]."
     brew install "$1"
 }
 
@@ -35,5 +37,5 @@ update_module direnv
 update_module git-delta
 update_module asdf
 update_module difftastic
-update_module micromamba
 update_module kubecolor
+update_module derailed/k9s
